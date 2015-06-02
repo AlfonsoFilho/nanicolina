8fb000fda8fbcb4f08a91535ab9f4ecbea385e59
Alfonso Filho <alfonsocfilho@gmail.com>
2015-06-02 00:50:13 -0300

    add support to ng-class

--- test/htmlParserSpec.js
+++ test/htmlParserSpec.js
@@ -28,6 +28,14 @@ describe('HTML Parser', function () {
     expect(htmlParser.parser('<h1>Test</h1>').html).to.be.function;
   });
 
+  it('should remove dot from class', function(){
+    expect(htmlParser.dropDot('.class')).to.be.equal('class');
+  });
+
+  it('should remove dot from class', function(){
+    expect(htmlParser.dropDot('.class')).to.be.equal('class');
+  });
+
   it('should replace class atributes', function(){
     var htmlSrc = [
       '<div class="classA"></div>',
@@ -44,34 +52,68 @@ describe('HTML Parser', function () {
     expect(htmlParser.replaceClass(tolkensMap, htmlSrc)).to.be.equal(expectedHtml);
   });
 
-  it.skip('should replace ng-class atributes', function(){
+  it('should replace ng-class atributes', function(){
     var htmlSrc = [
       '<div ng-class="classB"></div>',
-      '<div ng-class="{\'classB\': test()"></div>'
+      '<div ng-class="classA classB"></div>',
+      '<div ng-class="{\'classB\': test()}"></div>',
+      '<div ng-class="{\'classA\': test(), \'classB\': test()}"></div>',
+      '<div ng-class="[classA classB]"></div>',
+      '<div ng-class="[classA]"></div>'
     ].join();
     var expectedHtml = [
       '<div ng-class="b"></div>',
-      '<div ng-class="{\'b\': test()"></div>'
+      '<div ng-class="a b"></div>',
+      '<div ng-class="{\'b\': test()}"></div>',
+      '<div ng-class="{\'a\': test(), \'b\': test()}"></div>',
+      '<div ng-class="[classA classB]"></div>',
+      '<div ng-class="[classA]"></div>'
     ].join();
     var tolkensMap = { '.classA': 'a', '.classB': 'b' };
 
     expect(htmlParser.getRefactoredHTML(tolkensMap, htmlSrc)).to.be.equal(expectedHtml);
   });
 
-  it('should replace ng-class atributes', function(){
+  it('should replace data-ng-class atributes', function(){
+    var htmlSrc = [
+      '<div data-ng-class="classB"></div>',
+      '<div data-ng-class="classA classB"></div>',
+      '<div data-ng-class="{\'classB\': test()}"></div>',
+      '<div data-ng-class="{\'classA\': test(), \'classB\': test()}"></div>',
+      '<div data-ng-class="[classA classB]"></div>',
+      '<div data-ng-class="[classA]"></div>'
+    ].join();
+    var expectedHtml = [
+      '<div data-ng-class="b"></div>',
+      '<div data-ng-class="a b"></div>',
+      '<div data-ng-class="{\'b\': test()}"></div>',
+      '<div data-ng-class="{\'a\': test(), \'b\': test()}"></div>',
+      '<div data-ng-class="[classA classB]"></div>',
+      '<div data-ng-class="[classA]"></div>'
+    ].join();
+    var tolkensMap = { '.classA': 'a', '.classB': 'b' };
+
+    expect(htmlParser.getRefactoredHTML(tolkensMap, htmlSrc)).to.be.equal(expectedHtml);
+  });
+
+  it.skip('should replace class, ng-class and data-ng-class atributes', function(){
     var htmlSrc = [
       '<div class="classA"></div>',
       '<div class="classB"></div>',
       '<div class="classB classA"></div>',
-      // '<div ng-class="classB"></div>',
-      // '<div ng-class="{\'classB\': test()"></div>'
+      '<div ng-class="classB"></div>',
+      '<div ng-class="{\'classB\': test()"></div>',
+      '<div data-ng-class="classB" class="classA"></div>',
+      '<div class="classA classB classA" data-ng-class="[classA classB]"></div>'
     ].join();
     var expectedHtml = [
       '<div class="a"></div>',
       '<div class="b"></div>',
       '<div class="b a"></div>',
-      // '<div ng-class="b"></div>',
-      // '<div ng-class="{\'b\': test()"></div>'
+      '<div ng-class="b"></div>',
+      '<div ng-class="{\'b\': test()"></div>',
+      '<div data-ng-class="b" class="a"></div>',
+      '<div class="a b a" data-ng-class="[classA classB]"></div>'
     ].join();
     var tolkensMap = { '.classA': 'a', '.classB': 'b' };
 

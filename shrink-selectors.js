62907b3bd5ace1b8d16a6b8cb47b0cac8d68c898
Alfonso Filho <alfonsocfilho@gmail.com>
2015-06-06 14:29:12 -0300

    refactor core to use promise

--- shrink-selectors.js
+++ shrink-selectors.js
@@ -5,8 +5,9 @@ module.exports = function () {
 
   var fs = require('fs');
   var path = require('path');
+  var Q = require('q');
 
-  var utils = require('./lib/utils.js');
+  var utils = require('./lib/utils.js')();
   var htmlParser = require('./lib/htmlParser.js')();
   var cssParser = require('./lib/cssParser.js')();
   var tolkens = require('./lib/tolkens.js')();
@@ -18,44 +19,55 @@ module.exports = function () {
   var getHtmlFiles = filter(match(/\.html$/));
 
   var writeFiles = curry(function (dest, files) {
-    return forEach(function (file) {
-      utils.writeFile(path.join(dest || '', file.name), file.content);
+    return map(function (file) {
+      return utils.createFile(path.join(dest || '', file.name), file.content);
     }, files);
   });
 
 
   // Create Tolken Map
-  var createTolken = compose( // FilesList => TolkensMap
+  var createTolken = composeP( // FilesList => TolkensMap
       tolkens.getMap({}),
       join(''),
+      Q.all,
       map(utils.readFile),
       getCssFiles,
       prop('src'));
 
   // CSS
   var refactorCSS = curry(function (tolkensMap, src, dest) {
-    return compose(
+    return composeP(
+      Q.all,
       writeFiles(dest),
+      Q.all,
       map(function (file) {
-        return { name: file, content: cssParser.getRefactoredCSS(tolkensMap, utils.readFile(file)) };
+        return utils.readFile(file).then(function (content) {
+          return { name: file, content: cssParser.getRefactoredCSS(tolkensMap, content) };
+        });
       }),
       getCssFiles)(src);
   });
 
   //HTML
   var refactorHTML = curry(function (tolkensMap, src, dest) {
-    return compose(
+    return composeP(
+      Q.all,
       writeFiles(dest),
+      Q.all,
       map(function (file) {
-        return { name: file, content: htmlParser.getRefactoredHTML(tolkensMap, utils.readFile(file)) };
+        return utils.readFile(file).then(function (content) {
+          return { name: file, content: htmlParser.getRefactoredHTML(tolkensMap, content) };
+        });
       }),
       getHtmlFiles)(src);
   });
 
   var Shrink = function (options) {
-    var tolkensMap = createTolken(options);
-    refactorCSS(tolkensMap, options.src, options.dest);
-    refactorHTML(tolkensMap, options.src, options.dest);
+    return createTolken(options).then(function (tolkensMap) {
+      return Q.all(
+        refactorCSS(tolkensMap, options.src, options.dest),
+        refactorHTML(tolkensMap, options.src, options.dest));
+    });
   };
 
   return {
--- test/shrinkSelectorsSpec.js
+++ test/shrinkSelectorsSpec.js
@@ -2,12 +2,16 @@
 
 var assert = require("assert");
 var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
 var expect = chai.expect;
 var fs = require('fs');
 var path = require('path');
+var Q = require('q');
+
+chai.use(chaiAsPromised);
 
 var shrinkSelectors = require(path.resolve('shrink-selectors.js'))();
-var utils = require(path.resolve('lib', 'utils.js'));
+var utils = require(path.resolve('lib', 'utils.js'))();
 
 var fixturesPath = path.resolve('test', 'fixtures');
 var expectedPath = path.resolve('test', 'expected');
@@ -43,7 +47,8 @@ describe('Shrink Selectors', function () {
       '#main': 'm',
     };
 
-    expect(shrinkSelectors._createTolken({src: ['test/fixtures/test.css']})).to.be.deep.equal(expectedTolkenMap);
+    return expect(shrinkSelectors._createTolken({src: ['test/fixtures/test.css']})).to.eventually.be.deep.equal(expectedTolkenMap);
+
   });
 
   it('should filter css files', function () {
@@ -78,6 +83,90 @@ describe('Shrink Selectors', function () {
     expect(shrinkSelectors._getHtmlFiles(srcFiles)).to.be.deep.equal(expectedFiles);
   });
 
+
+  it('should write files', function (done) {
+
+    var files = [{name: 'test.txt', content: 'test'}];
+
+    Q.all(shrinkSelectors._writeFiles('output', files)).then(function (result) {
+      return Q.nfcall(fs.readFile, 'output/test.txt', {encoding: 'utf8'});
+    }).then(function (content) {
+      expect(content).to.be.equal('test');
+      done();
+    }).catch(function (err) {
+      done(err);
+    });
+
+  });
+
+  it('should refactor css files', function (done) {
+
+    var tolkenMap = {
+      '.content': 'a',
+      '.container': 'b',
+      '.hidden': 'c',
+      '.is-home': 'd',
+      '.row': 'e',
+      '.col-xs-12': 'f',
+      '.col-sm-8': 'g',
+      '.col-lg-4': 'h',
+      '.title': 'i',
+      '.link': 'j',
+      '.open': 'k',
+      '.visible-xs': 'l',
+      '#main': 'm',
+    };
+
+    var actualContent = '';
+
+    shrinkSelectors._refactorCSS(tolkenMap, ['test/fixtures/test.css'], 'output').then(function (result) {
+      return Q.nfcall(fs.readFile, 'output/test/fixtures/test.css', {encoding: 'utf8'});
+    }).then(function (content) {
+      actualContent = content;
+      return Q.nfcall(fs.readFile, 'test/expected/test.css', {encoding: 'utf8'});
+    }).then(function (content) {
+      expect(actualContent).to.be.deep.equal(content);
+      done();
+    }).catch(function (err) {
+      done(err);
+    });
+
+  });
+
+  it('should refactor html files', function (done) {
+
+    var tolkenMap = {
+      '.content': 'a',
+      '.container': 'b',
+      '.hidden': 'c',
+      '.is-home': 'd',
+      '.row': 'e',
+      '.col-xs-12': 'f',
+      '.col-sm-8': 'g',
+      '.col-lg-4': 'h',
+      '.title': 'i',
+      '.link': 'j',
+      '.open': 'k',
+      '.visible-xs': 'l',
+      '#main': 'm',
+    };
+
+    var actualContent = '';
+
+    shrinkSelectors._refactorHTML(tolkenMap, ['test/fixtures/test.html'], 'output').then(function (result) {
+      return Q.nfcall(fs.readFile, 'output/test/fixtures/test.html', {encoding: 'utf8'});
+    }).then(function (content) {
+      actualContent = content;
+      return Q.nfcall(fs.readFile, 'test/expected/test.html', {encoding: 'utf8'});
+    }).then(function (content) {
+      expect(actualContent).to.be.deep.equal(content);
+      done();
+    }).catch(function (err) {
+      done(err);
+    });
+
+  });
+
   it.skip('should mangle selectors', function () {
 
     shrinkSelectors.shrink({
@@ -92,20 +181,26 @@ describe('Shrink Selectors', function () {
       exportMapTo: ''
     });
 
-    var expectedCssA = fs.readFileSync(path.join(expectedPath, 'a.css'), {encoding: 'utf8'});
-    var expectedCssB = fs.readFileSync(path.join(expectedPath, 'b.css'), {encoding: 'utf8'});
-    var expectedHtmlA = fs.readFileSync(path.join(expectedPath, 'a.html'), {encoding: 'utf8'});
-    var expectedHtmlB = fs.readFileSync(path.join(expectedPath, 'b.html'), {encoding: 'utf8'});
+    // setTimeout(function  () {
 
-    var outputCssA = fs.readFileSync(path.join(outputPath, 'a.css'), {encoding: 'utf8'});
-    var outputCssB = fs.readFileSync(path.join(outputPath, 'b.css'), {encoding: 'utf8'});
-    var outputHtmlA = fs.readFileSync(path.join(outputPath, 'a.html'), {encoding: 'utf8'});
-    var outputHtmlB = fs.readFileSync(path.join(outputPath, 'b.html'), {encoding: 'utf8'});
+    //   var expectedCssA = fs.readFileSync(path.join(expectedPath, 'a.css'), {encoding: 'utf8'});
+    //   var expectedCssB = fs.readFileSync(path.join(expectedPath, 'b.css'), {encoding: 'utf8'});
+    //   var expectedHtmlA = fs.readFileSync(path.join(expectedPath, 'a.html'), {encoding: 'utf8'});
+    //   var expectedHtmlB = fs.readFileSync(path.join(expectedPath, 'b.html'), {encoding: 'utf8'});
 
-    expect(outputCssA).to.be.equal(expectedCssA);
-    expect(outputCssB).to.be.equal(expectedCssB);
-    expect(outputHtmlA).to.be.equal(expectedHtmlA);
-    expect(outputHtmlB).to.be.equal(expectedHtmlB);
+    //   var outputCssA = fs.readFileSync(path.join(outputPath, 'a.css'), {encoding: 'utf8'});
+    //   var outputCssB = fs.readFileSync(path.join(outputPath, 'b.css'), {encoding: 'utf8'});
+    //   var outputHtmlA = fs.readFileSync(path.join(outputPath, 'a.html'), {encoding: 'utf8'});
+    //   var outputHtmlB = fs.readFileSync(path.join(outputPath, 'b.html'), {encoding: 'utf8'});
+
+    //   expect(outputCssA).to.be.equal(expectedCssA);
+    //   expect(outputCssB).to.be.equal(expectedCssB);
+    //   expect(outputHtmlA).to.be.equal(expectedHtmlA);
+    //   expect(outputHtmlB).to.be.equal(expectedHtmlB);
+
+
+    //   done();
+    // }, 1000);
 
   });
 });
